ac1982bae0a507ffc7d1c8bf7599b8ba1c308058
chris <serhcli@gmail.com>
2016-09-24 13:34:14 +0000

    more additions

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # run in top level repo directory
 # assumes ubuntu
 
-# copy configs abd scripts
+# copy configs and scripts
 cp -r src/.  ~
 cp -R scripts ~
 cp -R xmonad ~
@@ -20,3 +20,36 @@ git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ~/.vi
 sudo apt-get -y install silversearcher-ag
 sudo apt-get -y install postgresql
 sudo apt-get -y install xmonad
+
+### languages and environments ###
+
+# ruby / rbenv setup
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+cd ~/dotfiles
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# haskell / stack setup
+sudo apt-get -y install ghc
+wget -qO- https://get.haskellstack.org/ | sh
+
+# node / npm setup
+sudo apt-get -y install nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get -y install npm
+
+
+### qol application setup ###
+
+# chrome repo
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+# spotify repo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# update, install
+sudo apt-get update
+sudo apt-get install spotify-client
+sudo apt-get install google-chrome-stable
