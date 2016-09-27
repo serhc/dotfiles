b3cf37ea3586522c19fe4668a74bcbc94948f1c0
chris <chris.liang@listenfirstmedia.com>
2016-09-27 15:27:36 -0400

    fix vim plugin dir

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo apt-get -y install gnome-vim
 mkdir ~/.vim
 mkdir ~/.vim/swps
 mkdir ~/.vim/plugins
-cp vim_plugins/* ~/.vim/plugins
+cp vim_plugins/* ~/.vim/plugin
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ~/.vim/
 
